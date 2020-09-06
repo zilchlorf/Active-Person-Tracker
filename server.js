46584080596054f9f3d76cb593e853dd80406ede
Max Edmonds <maxedmonds05@gmail.com>
2020-09-06 14:34:49 -0700

    deploying

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.static("public"));
 app.use("/", require("./routes/htmlRoutes"));
 app.use("/api", require("./routes/apiRoutes"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true,    useCreateIndex: true,
+useFindAndModify: false });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
